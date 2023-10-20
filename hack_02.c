a2c9b3770c2bc7c76d11b16c50c712447aab7d28
= <=>
2023-10-20 10:27:12 -0700

    Create another global variable name Result and initialized value 0

--- hack_02.c
+++ hack_02.c
@@ -7,7 +7,7 @@
 #include<stdio.h>
 #include<math.h>
 //globally decleare variable 
- double lon1,p1,lon2,p2,delta,Radius = 62371;
+ double lon1,p1,lon2,p2,delta,Radius = 62371,Result=0;
 //Function to convert Degree into Radian
 int Degree_to_Radian(int num)
 {
