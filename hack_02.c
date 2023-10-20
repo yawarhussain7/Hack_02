47460f69fa30363201f833402af26ea78becdea3
= <=>
2023-10-20 10:11:19 -0700

    Make variable on global scop and Show massage for user to enter their input in Degree

--- hack_02.c
+++ hack_02.c
@@ -5,11 +5,20 @@
 
 */
 #include<stdio.h>
-
+//globally decleare variable 
+ double lon1,p1,lon2,p2,delta;
 int main()
 {
-    double lon1,p1,lon2,p2,delta;
+   
 
+    printf("Enter the longitude of Origin in Degree [ -180 to 180 ] : ");
+    scanf("%lf",&lon1);
+    printf("Enter the latitude of Origin in Degree [ -90 to 90 ] : ");
+    scanf("%lf",&p1);
+    printf("Enter the longitude of Origin in Degree [ -180 to 180 ] : ");
+    scanf("%lf",&lon2);
+    printf("Enter the latitude of Distination in Degree [ -90 to 90 ] : ");
+    scanf("%lf",&p2);
     
 
     return 0;
